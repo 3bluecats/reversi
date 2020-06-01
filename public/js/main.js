c2d05f62d1a44df5c394613e81b0c6ab4d3a8aa3
caryn <cuetoc1@uci.edu>
2020-06-01 02:00:17 -0700

    InviteButton_
    Lobby: Basic Setup 02 assignment

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-/* functions for general use */
+    /* functions for general use */
 
-/* This function returns the value associated with "whichParam" on the URL*/
+    /* This function returns the value associated with "whichParam" on the URL*/
 function getURLParameters (whichParam)
 {
     var pageURL= window.location.search.substring(1);
@@ -18,24 +18,108 @@ if("undefined" ==typeof username || !username){
     username= "Anonymous_"+Math.random();
 }
 
-var chat_room='One_Room';
+var chat_room= getURLParameters("game_id");
+if ("undefined" == typeof chat_room || !chat_room){
+    chat_room="lobby";
+}
 
-/* Connect to the socket server */
+    /* Connect to the socket server */
 
 var socket = io.connect();
 
+    /* What to do when the server sends me a log message */
 
 socket.on("log",function(array){
     console.log.apply(console,array);
 });
+
+    /* What to do when the server responds that someone joined a room */
 
 socket.on('join_room_response',function(payload){
     if(payload.result=='fail'){
         alert(payload.message);
         return;
     }
-    $("#messages").append("<p>New user joined the room: "+payload.username+"</p>");
-});
+
+    /* If we are being notified that we joined the room, then ignore it */
+    if (payload.socket_id==socket.id){
+        return;
+    }
+
+    /* If someone joined then add a new row to the lobby table */
+    var dom_elements = $(".socket_"+payload.socket_id);
+
+    /* If we don't already have an entry for this person */
+    if(dom_elements.length == 0){
+        var nodeA= $("<div></div>");
+        nodeA.addClass("socket_"+payload.socket_id);
+        var nodeB = $("<div></div>");
+        nodeB.addClass("socket_"+payload.socket_id);
+        var nodeC = $("<div></div>");
+        nodeC.addClass("socket_"+payload.socket_id);
+
+        nodeA.addClass("w-100");
+        nodeB.append("<h4>"+payload.username+"</h4>");
+        nodeC.addClass("cold-3 text-left");
+        var buttonC = makeInviteButton();
+        nodeC.append(buttonC);
+
+        nodeA.hide();
+        nodeB.hide();
+        nodeC.hide();
+        $("#players").append(nodeA,nodeB,nodeC);
+        nodeA.slideDown(1000);
+        nodeB.slideDown(1000);
+        nodeC.slideDown(1000);
+    }
+    else{
+        var buttonC = makeInviteButton();
+        $(".socket_"+payload.socket_id+"button").replaceWith(buttonC);
+        dom_elements.slideDown(1000);
+    }
+
+    /* Manage the message that new player has joined */
+
+    var newHTML = "<p>" +payload.username+" just entered the lobby</p>" ;
+    var newNode = $(newHTML);
+    newNode.hide();
+    $("#messages").append(newNode);
+    newNode.slideDown(1000);
+}); 
+
+    /* What do do when the server says someone has left the room */
+
+ socket.on('player_disconnected',function(payload){
+    if(payload.result=='fail'){
+        alert(payload.message);
+        return;
+    }
+
+    /* If we are being notified that we left the room, then ignore it */
+    if (payload.socket_id==socket.id){
+        return;
+    }
+
+    /* If someone left the then animate out all their content */
+    var dom_elements = $(".socket_"+payload.socket_id);
+
+    /* If something exists */
+
+    if (dom_elements.length ==0){
+            dom_elements.slideUp(1000);
+    }
+       
+
+    /* Manage the message that a player has left */
+
+    var newHTML = "<p>" +payload.username+" just entered the lobby</p>";
+    var newNode = $(newHTML);
+    newNode.hide();
+    $("#messages").append(newNode);
+    newNode.slideDown(1000);
+}); 
+
+
 
 socket.on("send_message_response",function(payload){
     if (payload.result=="fail"){
@@ -63,11 +147,11 @@ $(function(){
     socket.emit('send_message',payload); 
 });
 
-
-
-
-
-
+function makeInviteButton(){
+    var newHTML = "<button type=\"button\" class=\"btn-outline-primary\">Invite</button>";
+    var newNode = $(newHTML);
+    return (newNode);
+}
 
 $(function(){
     var payload= {};
